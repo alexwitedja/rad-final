9f95175a66e45fc804d6341ad1ed6860667d5898
alexwitedja <s3641938@student.rmit.edu.au>
2020-06-12 23:36:23 +1000

    heroku deployment

--- config/puma.rb
+++ config/puma.rb
@@ -18,6 +18,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
